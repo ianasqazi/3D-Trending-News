06a42ce9b816d8a6fa9a3b9c138a139049703da2
Anas Qazi <ianasqazi@gmail.com>
2019-11-02 20:39:03 -0400

    appending row to main block

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -17,14 +17,20 @@ function callAPI(){
 
 
     // calling NEWS API 
-    var queryNEWSapi="https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=ed9bdb8084cb45b3bc6a7ead43be2e8d";
+    var query = $.trim($("#filter").val());
+
+    var queryNEWSapi="https://newsapi.org/v2/top-headlines?q="+query+"&apiKey=ed9bdb8084cb45b3bc6a7ead43be2e8d";
     $.ajax({
         url: queryNEWSapi,
         method: "GET"
         }).then(function(response) {
-            $(".main").append("<div id=api1>API1 details go here</div>");
-            $("#api1").append("<p>Response</p>");
-            $("#api1").text(JSON.stringify(response));
+
+            for (i=1;i<3;i++){
+                $(".main").append("<div id=api"+i+"block>API"+i+" details go here</div>");
+                $("#api"+i).addClass("row text-center");
+
+            }
+
             
         });
     
